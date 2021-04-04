739ae1abd06101f854c4d6835251d9e2d260d8b2
ismairdrs <ismairdrs@outlook.com>
2021-04-04 09:07:10 -0300

    fix

--- core/models/pedido.py
+++ core/models/pedido.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Pedido(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    usuario_id = models.CharField(max_length=100)
-    endereco_id = models.CharField(max_length=100)
-    pizza_id = models.CharField(max_length=100)
+    usuario_id = models.UUIDField(default=uuid.uuid4, editable=True)
+    endereco_id = models.UUIDField(default=uuid.uuid4, editable=True)
+    pizza_id = models.IntegerField()
     pedido_entregue = models.BooleanField(default=False)
