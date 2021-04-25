0bdb8a1b437c52ff96b2b9667ebb1d82d9a29a53
ismairdrs <ismairdrs@outlook.com>
2021-04-25 11:29:15 -0300

    refatorando websocket

--- core/api/v1/urls.py
+++ core/api/v1/urls.py
@@ -1,19 +1,14 @@
 from django.conf.urls import url, include
-#from rest_framework_nested import routers
 from rest_framework import routers
-from django.urls import path
 from core.api.v1 import views
 from core.api.v1.views.pedido import PedidoNaoEntregueViewSet
-from core.api.v1.views.websocket import websocket, WebsocketAPIView
+from core.api.v1.views.websocket import WebsocketAPIView
 
 router = routers.DefaultRouter()
-
 router.register('restaurante', PedidoNaoEntregueViewSet, basename='pedidos-restaurante')
 router.register('pedido', views.PedidoViewSet, basename='pedido')
 
-# url(r'.*', Gateway.as_view()),
 urlpatterns = [
     url(r'websocket/', WebsocketAPIView.as_view(), name='websocket'),
     url('', include(router.urls)),
-
 ]
--- core/api/v1/views/websocket.py
+++ core/api/v1/views/websocket.py
@@ -2,152 +2,68 @@ import time
 import pusher
 from core.models import Pedido
 from rest_framework import status
-from rest_framework.decorators import api_view
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class WebsocketAPIView(APIView):
-    def get(self, request, format=None):
-        STATUS = [
-            'Preparando envio do pedido',
-            'Pedido recebido pelo restaurante',
-            'Sua pizza está sendo preparada...',
-            'Pizza pronta! Saindo para a entrega...',
-            'Seu pedido está chegando...',
-            'Pedido Entregue!'
-        ]
+class WebsocketConnection():
+    _STATUS = [
+        'Preparando envio do pedido',
+        'Pedido recebido pelo restaurante',
+        'Sua pizza está sendo preparada...',
+        'Pizza pronta! Saindo para a entrega...',
+        'Seu pedido está chegando...',
+        'Pedido Entregue!'
+    ]
 
-        def create_connection():
-            pusher_client = pusher.Pusher(
-                app_id='1193493',
-                key='be00d08867dbcda17eb3',
-                secret='693bb2a1cba35d01c782',
-                cluster='mt1',
-                ssl=True
-            )
-            return pusher_client
+    def criar_conecao(self):
+        self.pusher_client = pusher.Pusher(
+            app_id='1193493',
+            key='be00d08867dbcda17eb3',
+            secret='693bb2a1cba35d01c782',
+            cluster='mt1',
+            ssl=True
+        )
+        return self.pusher_client
 
-        def send_message(pusher_client):
+    def enviar_status(self):
+        time.sleep(1)
+        for status in self._STATUS:
+            self.pusher_client.trigger('my-channel', 'my-event', {'message': status})
             time.sleep(1)
-            for status in STATUS:
-                pusher_client.trigger('my-channel', 'my-event', {'message': status})
-                time.sleep(1)
 
-        def mudar_status_pedido(id):
-            pedido = Pedido.objects.get(id=id)
-            if pedido.pedido_entregue:
-                return False
-            else:
-                pedido.pedido_entregue = True
-                pedido.save()
 
+class WebsocketAPIView(APIView):
+    def get(self, request, form=None):
         id = self.request.query_params.get('id')
-        try:
-            mudar_status_pedido(id)
-        except:
+        pedido = self.encontrar_pedido(id)
+
+        if not pedido:
             content = {'Requisição inválida': 'ID do pedido inválido'}
             return Response(content, status=status.HTTP_400_BAD_REQUEST)
-        pusher_client = create_connection()
-        send_message(pusher_client)
+
+        if pedido.pedido_entregue:
+            content = {'Status pedido': 'Pedido Entregue'}
+            return Response(content, status=status.HTTP_200_OK)
+
+        self.enviar_status_websocket()
+        self.alterar_status_pedido(pedido)
 
         data = {"websocket": "success"}
         return Response(data, status=status.HTTP_200_OK)
 
-
-
-
-@api_view(['GET'])
-def websocket(request):
-    STATUS = [
-        'Preparando envio do pedido',
-        'Pedido recebido pelo restaurante',
-        'Sua pizza está sendo preparada...',
-        'Pizza pronta! Saindo para a entrega...',
-        'Seu pedido está chegando...',
-        'Pedido Entregue!'
-    ]
-
-    def create_connection():
-        pusher_client = pusher.Pusher(
-            app_id='1193493',
-            key='be00d08867dbcda17eb3',
-            secret='693bb2a1cba35d01c782',
-            cluster='mt1',
-            ssl=True
-        )
-        return pusher_client
-
-    def send_message(pusher_client):
-        time.sleep(5)
-        for status in STATUS:
-            pusher_client.trigger('my-channel', 'my-event', {'message': status})
-            time.sleep(5)
-
-    def mudar_status_pedido(id):
+    def encontrar_pedido(self, id):
         try:
-            pedido = Pedido.objects.get(id=id)
+            return Pedido.objects.get(id=id)
         except:
-            content = {'Requisição inválida': 'ID do pedido inválido'}
-            #raise ValidationError(content, status=status.HTTP_400_BAD_REQUEST)
-            pass
-        else:
-            pedido.pedido_entregue = True
-            pedido.save()
+            return False
 
-    def main():
-        mudar_status_pedido(id)
-        pusher_client = create_connection()
-        send_message(pusher_client)
+    def enviar_status_websocket(self):
+        websocket = WebsocketConnection()
+        websocket.criar_conecao()
+        websocket.enviar_status()
 
-    main()
-    data= {"websocket": "success"}
-    return Response(data, status=status.HTTP_200_OK)
-
-@api_view(['GET'])
-def websocket(request):
-    STATUS = [
-        'Preparando envio do pedido',
-        'Pedido recebido pelo restaurante',
-        'Sua pizza está sendo preparada...',
-        'Pizza pronta! Saindo para a entrega...',
-        'Seu pedido está chegando...',
-        'Pedido Entregue!'
-    ]
-
-    def create_connection():
-        pusher_client = pusher.Pusher(
-            app_id='1193493',
-            key='be00d08867dbcda17eb3',
-            secret='693bb2a1cba35d01c782',
-            cluster='mt1',
-            ssl=True
-        )
-        return pusher_client
-
-    def send_message(pusher_client):
-        time.sleep(5)
-        for status in STATUS:
-            pusher_client.trigger('my-channel', 'my-event', {'message': status})
-            time.sleep(5)
-
-    def mudar_status_pedido(id):
-        try:
-            pedido = Pedido.objects.get(id=id)
-        except:
-            content = {'Requisição inválida': 'ID do pedido inválido'}
-            #raise ValidationError(content, status=status.HTTP_400_BAD_REQUEST)
-            pass
-        else:
-            pedido.pedido_entregue = True
-            pedido.save()
-
-    def main():
-        mudar_status_pedido(id)
-        pusher_client = create_connection()
-        send_message(pusher_client)
-
-    main()
-    data= {"websocket": "success"}
-    return Response(data, status=status.HTTP_200_OK)
+    def alterar_status_pedido(self, pedido):
+        pedido.pedido_entregue = True
+        pedido.save()
